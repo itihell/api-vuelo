eeb5f9d62cb9c34f30498e1cea5c0f904867677b
Itihell Mejia <itihell.mejia@gmail.com>
2021-11-24 11:01:20 -0600

    CRUD de pajeros

--- src/pasajero/pasajero.controller.ts
+++ src/pasajero/pasajero.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { create } from 'domain';
+import { PasajeroDTO } from './dto/pasajero.dto';
+import { PasajeroService } from './pasajero.service';
 
-@Controller('pasajero')
-export class PasajeroController {}
+@Controller('api/v1/pasajero')
+export class PasajeroController {
+  constructor(private readonly pasajeroService: PasajeroService) {}
+  @Post()
+  create(@Body() campos: PasajeroDTO) {
+    return this.pasajeroService.create(campos);
+  }
+
+  @Get()
+  findAll() {
+    return this.pasajeroService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.pasajeroService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() campos: PasajeroDTO) {
+    return this.pasajeroService.update(id, campos);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.pasajeroService.delete(id);
+  }
+}
--- src/pasajero/pasajero.module.ts
+++ src/pasajero/pasajero.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PASAJERO } from 'src/common/models/models';
 import { PasajeroController } from './pasajero.controller';
 import { PasajeroService } from './pasajero.service';
+import { PasajeroSchema } from './schema/pasajero.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: PASAJERO.name,
+        useFactory: () => PasajeroSchema,
+      },
+    ]),
+  ],
   controllers: [PasajeroController],
-  providers: [PasajeroService]
+  providers: [PasajeroService],
 })
 export class PasajeroModule {}
