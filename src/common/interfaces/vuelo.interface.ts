3c8bddb48ac4f171099c1d2e18fc3d38ead691e5
Itihell Mejia <itihell.mejia@gmail.com>
2021-11-25 14:59:42 -0600

    Conectando a la apip del clima para los vuelos

--- src/common/interfaces/vuelo.interface.ts
+++ src/common/interfaces/vuelo.interface.ts
@@ -2,6 +2,7 @@ import { ClimnaInterface } from './clima.interface';
 import { PasajeroInterface } from './pasajero.interface';
 
 export interface VueloInterface extends Document {
+  _id?: string;
   piloto: string;
   avion: string;
   destino: string;
--- src/vuelo/vuelo.service.ts
+++ src/vuelo/vuelo.service.ts
@@ -4,12 +4,52 @@ import { Model } from 'mongoose';
 import { VueloInterface } from 'src/common/interfaces/vuelo.interface';
 import { VUELO } from 'src/common/models/models';
 import { VueloTDO } from './dto/vuelo.tdo';
+import axios from 'axios';
+import * as moment from 'moment';
+import { UbicacionInterface } from '../common/interfaces/ubicacion.interface';
+import { ClimnaInterface } from 'src/common/interfaces/clima.interface';
 
 @Injectable()
 export class VueloService {
   constructor(
     @InjectModel(VUELO.name) private readonly model: Model<VueloInterface>,
   ) {}
+
+  async getUbicacion(destino: string): Promise<UbicacionInterface> {
+    const { data } = await axios.get(
+      `https://www.metaweather.com/api/location/search/?query=${destino}`,
+    );
+
+    return data[0];
+  }
+  async getClima(woeid: number, fecha: Date): Promise<ClimnaInterface[]> {
+    const dateForma = moment.utc(fecha).format();
+    const anio = dateForma.substring(0, 4);
+    const mes = dateForma.substring(5, 7);
+    const dia = dateForma.substring(8, 10);
+
+    const { data } = await axios.get(
+      `https://www.metaweather.com/api/location/${woeid}/${anio}/${mes}/${dia}/`,
+    );
+
+    return data;
+  }
+
+  assign(
+    { _id, piloto, avion, destino, fecha, pasajeros }: VueloInterface,
+    clima: ClimnaInterface[],
+  ): VueloInterface {
+    return Object.assign({
+      _id,
+      piloto,
+      avion,
+      destino,
+      fecha,
+      pasajeros,
+      clima,
+    });
+  }
+
   async create(campos: VueloTDO): Promise<VueloInterface> {
     const newVuelo = new this.model(campos);
     return await newVuelo.save();
@@ -20,7 +60,17 @@ export class VueloService {
   }
 
   async findOne(id: string): Promise<VueloInterface> {
-    return await this.model.findById(id).populate('pasajeros');
+    const vuelo = await this.model.findById(id).populate('pasajeros');
+
+    const ubicacion: UbicacionInterface = await this.getUbicacion(
+      vuelo.destino,
+    );
+
+    const clima: ClimnaInterface[] = await this.getClima(
+      ubicacion.woeid,
+      vuelo.fecha,
+    );
+    return this.assign(vuelo, clima);
   }
 
   async update(id: string, campos: VueloTDO): Promise<VueloInterface> {
