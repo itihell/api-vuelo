17c948c78b885c228e8f782ead41268b2a642632
Itihell Mejia <itihell.mejia@gmail.com>
2021-11-25 15:46:54 -0600

    Agregando barra de busqueda en la documentacion

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
 
-  SwaggerModule.setup('/api/docs', app, document);
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
 
   await app.listen(3000);
 }
