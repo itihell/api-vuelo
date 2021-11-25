97da4b4cca3c454574e9e8771512519c4f08def3
Itihell Mejia <itihell.mejia@gmail.com>
2021-11-25 15:15:55 -0600

    Instalando y configurando Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Api de Vuelos')
+    .setDescription('Simple aplicacion de vuelos programados')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
